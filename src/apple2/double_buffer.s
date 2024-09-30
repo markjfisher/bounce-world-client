f66840075f5e8bd0f9eeedca21b135a063fa833b
Mark Fisher <mark.j.fisher@gmail.com>
2024-09-30 10:52:36 +0100

    apple2 - simpler show_other_screen and swap_buffer

--- src/apple2/double_buffer.s
+++ src/apple2/double_buffer.s
@@ -12,32 +12,16 @@ SHOW_SCR1 = $C054
 SHOW_SCR2 = $C055
 
 .proc   _show_other_screen
-        lda     _is_orig_screen_mem
-        beq     swap_to_screen2
-
-.if (.cpu .bitand ::CPU_ISET_65SC02)
-        stz     SHOW_SCR1
-.else
-        lda     #$00
-        sta     SHOW_SCR1
-.endif
+        ldx     _is_orig_screen_mem
+        lda     SHOW_SCR1,x
         rts
-
-swap_to_screen2:
-        sta     SHOW_SCR2
-        rts
-
 .endproc
 
 ; we only need to swap the flag value on apple2, the TEXTLN_HI value has to be changed after a goto conio function, so a lot more frequently than atari
 .proc   _swap_buffer
-        ; x = 1 - x for binary value swaps between 0 and 1.
-        ; On 6502, this is quicker than doing test for 1 or 0 and then swapping it (12 + 13) / 2
-        ; On 65c02 this is 1 cycle slower on average (10 + 12) / 2, so take the hit for simpler code
-        sec                                     ; 2
-        lda     #$01                            ; 2 (4)
-        sbc     _is_orig_screen_mem             ; 4 (8)
-        sta     _is_orig_screen_mem             ; 4 (12)
+        lda     _is_orig_screen_mem
+        eor     #1
+        sta     _is_orig_screen_mem
         rts
 .endproc
 
