7db90bec402a08d7ccc8fce42511f5ff0d8b0b57
Rich Stephens <richswww@gmail.com>
2026-06-12 16:58:36 -0400

    Update to use newline in messages for new server; DOUBLE BUFFERING

--- src/coco/shutdown.c
+++ src/coco/shutdown.c
@@ -2,8 +2,9 @@
 #include <coco.h>
 #include <conio_wrapper.h>
 
-void cleanup_client() 
+void cleanup_client()
 {
+    shutdown_double_buffer();
     closeHiResTextScreen();
     pmode(0, 0);
     screen(0, 0);
